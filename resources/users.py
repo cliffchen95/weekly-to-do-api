5e1aad06388eea507dd35e9c5f8a9fd6ef9d337d
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-30 10:37:44 -0400

    log in working

--- resources/users.py
+++ resources/users.py
@@ -3,8 +3,8 @@ import models
 User = models.User
 
 from flask import Blueprint, request, jsonify
-from flask_bcrypt import generate_password_hash
-from flask_login import login_user
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_login import login_user, current_user
 
 from playhouse.shortcuts import model_to_dict
 
@@ -45,3 +45,45 @@ def user():
 
   if request.method == 'DELETE':
     return "you hit delete route"
+
+
+@users.route('/login', methods=['POST'])
+def login():
+  if not current_user.is_authenticated:
+    payload = request.get_json()
+    payload['username'] = payload['username'].lower()
+
+    try:
+      user = User.get(User.username == payload['username'])
+      user_dict = model_to_dict(user)
+      if check_password_hash(user_dict['password'], payload['password']):
+        
+        login_user(user)
+
+        user_dict.pop('password')
+        return jsonify(
+          data=user_dict,
+          message=f"Successfully logged in {user_dict['username']}",
+          status=200
+        ), 200
+      else:
+        return jsonify(
+          data={},
+          message='Username does not exist or incorrect password',
+          status=401
+        ), 401
+
+    except models.DoesNotExist:
+      return jsonify(
+        data={},
+        message='Username does not exist or incorrect password',
+        status=401
+      ), 401
+  else:
+    user = model_to_dict(current_user)
+    user.pop('password')
+    return jsonify(
+      data=user,
+      message=f"You are currently logged in as {user['username']}",
+      status=200
+    ), 200
